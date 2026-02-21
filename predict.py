9a1835781b2363955dc031e7c31bf17e3ed4af54
Karan Bhise <karanbhise126@gmail.com>
2026-02-21 20:07:26 +0530

    fix model download

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ import requests
 import tflite_runtime.interpreter as tflite
 
 MODEL_PATH = "models/model.tflite"
-MODEL_URL = "https://github.com/Karanaldo-07/ball-bearing-defect-detection/releases/download/v1/model.tflite"
+MODEL_URL = "https://github.com/Karanaldo-07/ball-bearing-defect-detection/releases/download/model/model.tflite"
 
 
 def download_model():
